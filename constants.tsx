9fbcb3afcba94780d5d3c0d4b2414453d545e242
jm <60770218+JohnMichaelCantara@users.noreply.github.com>
2026-01-27 11:32:37 +0800

    feat: Update logo URLs
    Replaces outdated Google Drive links for application logos with new, active URLs. This ensures the logos display correctly within the Student Organization Portal.

--- constants.tsx
+++ constants.tsx
@@ -2,9 +2,9 @@
 import { User, Event, Announcement, GalleryItem, Notification, UserRole } from './types';
 
 export const LOGO_URLS = [
-  "https://lh3.googleusercontent.com/sitesv/AAzXCkeOQo1EYr41OxaY5eflCs8nzXQWJOa7CKb9BIRYpnjP3BQhR5YrvDykgBMRqugNp53Q-kMwhyiekZ9tcTvYoFsc79p0vaYkll7U-hO6Eso0CAqJ3XWeuKOIo-Po4pOPVGKjlBGzIRm9T_kIiJnHfT6yq_Rl7xY6T3OgHl1dCQ6gerM3DPej6FnBkEE=w1280",
-  "https://lh3.googleusercontent.com/sitesv/AAzXCkd9JBXGqkDOavqTuQMVMrAJLULWnRLbkdDEqUdXmT3p14vvwaWytcTn1HnlX52DGJU5bfuUpXIMVy-7pow0JwrfmnKpZRYXXlB2SX_SkiwJViMv6XCo1_Pb2DJ4U9-kHWrMi68juvqen6_J4qaYOR5euW4yB6w6bt4aesZ3MQwdM7QSxsLxMJx6VRxqx6ELk-OKQXGafrY-RLIlluIaEAgrXvah1yF2q6vc6L4=w1280",
-  "https://lh3.googleusercontent.com/sitesv/AAzXCkfbztjEa85XQ__opUwdN6nWsQ5ZKgp9-TZ4OzIEKWlJSSayEM_2K0ai_kK1QQx0dnKuwdV-BKmrOEa5GgTPa_QppFfLF4nX6gbTvwqGVAUQEimSfwIm3jrKyMEnA86B21tFVjSz_NJZH-fV3OtF2eKqkneQQr-luthMjZ1dmESWxxhKCLc5XKjB_nA-JcLqO31fGubJ4Ct1RJH2i2lst1AuV5mubCoI4wTk-es=w1280"
+  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7qicFPTS-UWi0LEZuhpGiJJG83t-QoHRfIPauWuDTCeaoVw66QqvvJu7BBFD1Xn8APW-ROH1XTzE0CXvo3xcGgM3J2WNFvmqkzQU4YoEA9Ds-j9oWPo-xtmiJDhvA7Gt5TOd-Q8A8qbn6cZUu1VFERA8i7ouDvnPDIUDrwfEDizBCm7ixKhBNNjYZBSu8IN?key=W_u8fiN7_oF3X6TwKcWtFg",
+  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7oIZssGP4dBBP1_8zdoIW3L9PtBUyU4IG1nmTh3IWmRXVC-4ETzQps7VsE2Q9g2Czc_13cnbv1t7BnjAwaYtOsb9qwZYt0pqAzhP0jT0WAfGkB5JXi4iKzDlm9rugcMYJY6v6nHkEFPPdnvYWsS5Mg_m0t0IFhWHYR9VqXWfC71BpsxnLpufxxdf47cVOtR?key=W_u8fiN7_oF3X6TwKcWtFg",
+  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7oUHrDmM01QgVN56mnAce1Er8WlNuhi-syXCBoG5jmzOlrnOmaK2a6ldUP3Jpz_ezyV2_TLZNdh-vJgDdIq5J_BOk2j0Ahy39u3d1Yu59gNYekUFl_StnPrXbCDMhhgqEe81MEjgBdGzoVMBNbOs15Vp5rprLTdtCJ88t5NIPqgr2oRk9-TNGyYAGPI_Z4c?key=W_u8fiN7_oF3X6TwKcWtFg"
 ];
 
 export const MOCK_USERS: User[] = [];
