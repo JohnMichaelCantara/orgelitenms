d0eb650de5b16165de1a1a63fbf5aa571faf834e
jm <60770218+JohnMichaelCantara@users.noreply.github.com>
2026-01-28 21:36:41 +0800

    feat: Update logo URLs in constants
    Replaced existing logo image URLs with new ones. These new URLs likely point to updated or different assets for the application's branding.

--- constants.tsx
+++ constants.tsx
@@ -2,9 +2,9 @@
 import { User, Event, Announcement, GalleryItem, Notification, UserRole } from './types';
 
 export const LOGO_URLS = [
-  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7qicFPTS-UWi0LEZuhpGiJJG83t-QoHRfIPauWuDTCeaoVw66QqvvJu7BBFD1Xn8APW-ROH1XTzE0CXvo3xcGgM3J2WNFvmqkzQU4YoEA9Ds-j9oWPo-xtmiJDhvA7Gt5TOd-Q8A8qbn6cZUu1VFERA8i7ouDvnPDIUDrwfEDizBCm7ixKhBNNjYZBSu8IN?key=W_u8fiN7_oF3X6TwKcWtFg",
-  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7oIZssGP4dBBP1_8zdoIW3L9PtBUyU4IG1nmTh3IWmRXVC-4ETzQps7VsE2Q9g2Czc_13cnbv1t7BnjAwaYtOsb9qwZYt0pqAzhP0jT0WAfGkB5JXi4iKzDlm9rugcMYJY6v6nHkEFPPdnvYWsS5Mg_m0t0IFhWHYR9VqXWfC71BpsxnLpufxxdf47cVOtR?key=W_u8fiN7_oF3X6TwKcWtFg",
-  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7oUHrDmM01QgVN56mnAce1Er8WlNuhi-syXCBoG5jmzOlrnOmaK2a6ldUP3Jpz_ezyV2_TLZNdh-vJgDdIq5J_BOk2j0Ahy39u3d1Yu59gNYekUFl_StnPrXbCDMhhgqEe81MEjgBdGzoVMBNbOs15Vp5rprLTdtCJ88t5NIPqgr2oRk9-TNGyYAGPI_Z4c?key=W_u8fiN7_oF3X6TwKcWtFg"
+  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7og2p7kMsIHGH2BlhnqWZCMzbeCRETHJZN8mS3UNrxrZD5s13e0_SL_rJkrHoto4zF9t0x66FDsBmF-ZJ4VNg2KwUqmOkMKUzEEmFgpT5ucPI8gV59T1vGEx9sqmiCrOZ57_atM29IhIC0q0bOxhABCJgPm3EjnpiG_q_VVH-VOR6Fwu49QoYNcmLIXkcOF?key=W_u8fiN7_oF3X6TwKcWtFg",
+  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7og0xVECOuvUJUdRkAXoonn-Cnlo2ZYoSYN4IEc00GOgPGsXtDKKmvHZ6wK-z_N0WY68w_ZqDoCRqOXtR6UVJObMH4Yc-L7uL32ynwO_kaTx5cTOdSuj4huBHK4JzRL-DEclFDsIDQvIljNldIQYzdwqOPRs9PrbK3-0qnHh2X1UJL3ws1Bymr9Gd319jVf?key=W_u8fiN7_oF3X6TwKcWtFg",
+  "https://lh7-rt.googleusercontent.com/sitesz/AClOY7o2dEJswiWaORavWpfqb6k4lsA2pLjd5OUe-ot87nB8XJvwDIlQG0fACdUQtH52uIv6QTmFYp4qYeIsMiawD8o9NTB5BflvsstPFCrR9hARJWKcCaHvsML9XYtoXcU0J311qcq8SvqwdHzk9od_6fgSSMczfXr0Q4BgOh-S0jrPtJaeUA9FxLeM6-I94C7h?key=W_u8fiN7_oF3X6TwKcWtFg"
 ];
 
 export const MOCK_USERS: User[] = [];
